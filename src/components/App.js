5cb5d21dfd4e5f79b6f6592ef12c1f6ee73ccedb
Serhii75 <serhii.ivanenko@gmail.com>
2017-10-27 13:26:27 +0300

    Init React Boilerplate commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import imgCode from '../images/binary_code.jpg'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Boilerplate for React</h1>
+        <img src={imgCode} />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+import './scss/base.scss'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
