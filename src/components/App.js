d0bfd0ebac3df921195a82a650953f059d006124
Serhii75 <serhii.ivanenko@gmail.com>
2018-03-30 16:17:21 +0300

    Modify webpack config for dev and prod versions

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Boilerplate for React</h1>
+        <h1>Boilerplate for React Apps</h1>
         <img src={imgCode} />
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
 import App from './components/App'
-
 import './scss/base.scss'
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!')
+}
 
 ReactDOM.render(
   <App />,
